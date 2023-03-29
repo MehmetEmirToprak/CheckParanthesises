ccb89fadad86448d27061d5e9e052bdbaa016b7d
MehmetEmirToprak <120792454+MehmetEmirToprak@users.noreply.github.com>
2023-03-29 23:52:52 +0300

    Add files via upload

---
+++ check-paranthesis.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct LINKED_STACK_NODE_s *LINKED_STACK_NODE;
+typedef struct LINKED_STACK_NODE_s {
+	LINKED_STACK_NODE next;
+	void *data;
+} LINKED_STACK_NODE_t[1];
+
+typedef struct LINKED_STACK_s {
+	LINKED_STACK_NODE head;
+} LINKED_STACK_t[1], *LINKED_STACK;
+
+int linked_stack_is_empty(LINKED_STACK stack) {
+	return stack->head == NULL;
+}
+
+LINKED_STACK linked_stack_init() {
+	LINKED_STACK stack = malloc(sizeof(LINKED_STACK_t));
+	stack->head = NULL;
+	return stack;
+}
+
+void linked_stack_push(LINKED_STACK stack, void *data) {
+	LINKED_STACK_NODE new = malloc(sizeof(LINKED_STACK_NODE_t));
+	if (new != NULL) {
+		new->data = data;
+		new->next = stack->head;
+		stack->head = new;
+	} else {
+		printf("Memory allocation error!\n");
+		exit(0);
+	}
+}
+
+void *linked_stack_pop(LINKED_STACK stack) {
+	LINKED_STACK_NODE temp;
+	void *result = NULL;
+	if (stack->head != NULL) {
+		temp = stack->head;
+		stack->head = temp->next;
+		result = temp->data;
+		free(temp);
+	} else {
+		printf("Stack is empty\n");
+		exit(0);
+	}
+	return result;
+}
+
+void linked_stack_free(LINKED_STACK stack) {
+	while (stack->head != NULL) {
+		linked_stack_pop(stack);
+	}
+	free(stack);
+}
+
+int balancing_symbols(char *string){
+	LINKED_STACK stack = linked_stack_init();
+	for(int i = 0; i < strlen(string);i++){
+		if(string[i] == '('){
+			linked_stack_push(stack, (void *)string[i]);
+		}
+		else if(string[i] == ')'){
+			if(stack->head != NULL){
+				linked_stack_pop(stack);
+			}
+			else{
+				return 0;
+			}
+		}
+
+	}
+	if(!linked_stack_is_empty(stack)){
+		return 0;
+	}
+	return 1;
+}
+int main() {
+	char *mystring = "(())";
+	int res = balancing_symbols(mystring);
+	if(res == 1){
+		printf("Parentheses match.\n");
+	}else{
+		printf("Parentheses do not match.\n");
+	}
+	return 0;
+}
